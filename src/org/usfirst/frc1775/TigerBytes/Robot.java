a401f434644ae4f67d295d72089fd2c39b98a14b
Caleb Hyde <caleb.hyde@gmail.com>
2017-10-30 20:57:13 -0500

    Initial commit of minimal robot

---
+++ src/org/usfirst/frc1775/TigerBytes/Robot.java
@@ -0,0 +1,49 @@
+package org.usfirst.frc1775.TigerBytes;
+
+import java.text.DecimalFormat;
+
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends IterativeRobot {
+	public static OI oi;
+	public static Subsystem sensors;
+
+	/**
+	 * This function is run when the robot is first started up and should be used
+	 * for any initialization code.
+	 */
+	public void robotInit() {
+		RobotMap.init();
+		sensors = new Subsystem() {
+			private final ADXRS450_Gyro gyro = RobotMap.sensorsGyro;
+
+			public void log() {
+		        double angle = 0;
+		        DecimalFormat f = new DecimalFormat("#0.00");
+
+		        while (true) {
+			    		angle = gyro.getAngle();
+						System.out.println(f.format(angle));
+						Timer.delay(.5);
+		        }
+			}
+
+			@Override
+			protected void initDefaultCommand() {
+				gyro.reset();
+			}
+		};
+
+		oi = new OI();
+	};
+}
