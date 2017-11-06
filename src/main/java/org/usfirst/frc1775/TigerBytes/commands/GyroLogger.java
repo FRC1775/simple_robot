107b3bb52178af0960af4846d3d2fd2819f5c991
Caleb Hyde <caleb.hyde@gmail.com>
2017-11-05 22:09:29 -0600

    Add log4j dependency and use it

--- src/main/java/org/usfirst/frc1775/TigerBytes/commands/GyroLogger.java
+++ src/main/java/org/usfirst/frc1775/TigerBytes/commands/GyroLogger.java
@@ -2,6 +2,10 @@ package org.usfirst.frc1775.TigerBytes.commands;
 
 import java.text.DecimalFormat;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.usfirst.frc1775.TigerBytes.Robot;
+
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
@@ -9,6 +13,7 @@ import edu.wpi.first.wpilibj.command.Command;
 public class GyroLogger extends Command {
 	private ADXRS450_Gyro gyro;
 	private DecimalFormat formatter;
+	private static final Logger log4j = LogManager.getLogger(Robot.class.getName());
 	
 	@Override
 	protected void initialize() {
@@ -20,8 +25,8 @@ public class GyroLogger extends Command {
 	@Override
 	protected void execute() {
         double angle = 0;
-    	angle = gyro.getAngle();
-		System.out.println(formatter.format(angle));
+    		angle = gyro.getAngle();
+		log4j.info(formatter.format(angle));
 		Timer.delay(.5);
 	}
 
